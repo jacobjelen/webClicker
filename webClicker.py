0ce3e097129d92fd851f3c3e5c1448c9c1abb772
jacobjelen <jacobjelen@gmail.com>
2021-01-17 16:49:23 +0000

    initial

---
+++ webClicker.py
@@ -0,0 +1,9 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+import time
+browser = webdriver.Safari(executable_path = '/usr/bin/safaridriver')
+browser.get('https://www.spareroom.com')
+time.sleep(10)
+browser.quit()
